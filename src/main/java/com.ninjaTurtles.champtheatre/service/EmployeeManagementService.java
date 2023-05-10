838433d59a521c880b47385816380ba897db96e7
peremmm <peradilla.emman08@gmail.com>
2023-05-10 14:35:19 +0800

    Added getAllEmployee IMPL edited service interface

--- src/main/java/com.ninjaTurtles.champtheatre/service/EmployeeManagementService.java
+++ src/main/java/com.ninjaTurtles.champtheatre/service/EmployeeManagementService.java
@@ -3,9 +3,9 @@ package com.ninjaTurtles.champtheatre.service;
 import com.ninjaTurtles.champtheatre.bean.EmployeeBean;
 import com.ninjaTurtles.champtheatre.models.Employee;
 import com.ninjaTurtles.champtheatre.models.EmployeeAccount;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
 
 public interface EmployeeManagementService {
 	
